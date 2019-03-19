ba1bb424e026210741bc6e9fc68f8a8f0c32a6e2
Tasuku Maeda <maeda.tasuku@classmethod.jp>
2019-03-19 13:35:27 +0900

    [add] addImage Example in LayerControllerExample

--- iOSExample/iOSExample/LayerController/LayerControllerExample.swift
+++ iOSExample/iOSExample/LayerController/LayerControllerExample.swift
@@ -15,6 +15,7 @@ class LayerControllerExample: UIViewController {
         
         appearance()
         transform()
+        addImages()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,7 +28,7 @@ class LayerControllerExample: UIViewController {
         layerController.setOpacity(0.5)
             .setCornerRadius(8)
             .setBackgroundColor(.red)
-            .setFrame(x: 100, y: 80, width: 100, height: 80)
+            .setFrame(x: 100, y: 20, width: 100, height: 80)
         
         canvasView.layer.addSublayer(layerController.layer)
     }
@@ -36,10 +37,24 @@ class LayerControllerExample: UIViewController {
         let layerController = LayerController()
         layerController.setBackgroundColor(.blue)
             .setFrame(x: 0, y: 0, width: 100, height: 80)
-            .translate(x: 100, y: 180)
+            .translate(x: 100, y: 100)
             .rotate(radian: .pi / 4)
             .scale(scaleX: 1.5, scaleY: 1.5)
         
         canvasView.layer.addSublayer(layerController.layer)
+    }
+    
+    func addImages() {
+        guard let smileImage = UIImage(named: "smile"),
+            let leftArrowImage = UIImage(named: "leftArrow") else { return }
+        let layerController = LayerController()
+        layerController.setImage(smileImage)
+            .translate(x: 10, y: 300)
+        canvasView.layer.addSublayer(layerController.layer)
+        
+        let layerController2 = LayerController()
+        layerController2.setImage(leftArrowImage)
+            .translate(x: 160, y: 300)
+        canvasView.layer.addSublayer(layerController2.layer)
     }
 }
