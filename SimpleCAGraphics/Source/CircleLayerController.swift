58209f8c54533ce76f8acaedb3348b203668d4fd
Tasuku Maeda <blogcontact@kuma-de.com>
2019-03-23 10:50:50 +0900

    add CGFloat argument to CircleLayerController

--- SimpleCAGraphics/Source/CircleLayerController.swift
+++ SimpleCAGraphics/Source/CircleLayerController.swift
@@ -17,14 +17,28 @@ public class CircleLayerController: LayerController<CAShapeLayer> {
         drawCircle(x: x, y: y, radius: radius)
     }
     
+    convenience public init(cgX: CGFloat, cgY: CGFloat, cgRadius: CGFloat) {
+        self.init()
+        drawCircleCG(x: cgX, y: cgY, radius: cgRadius)
+    }
+    
     @discardableResult
     public func drawCircle(x: Double, y: Double, radius: Double) -> Self {
+        return _drawCircle(x: CGFloat(x), y: CGFloat(y), radius: CGFloat(radius))
+    }
+    
+    @discardableResult
+    public func drawCircleCG(x: CGFloat, y: CGFloat, radius: CGFloat) -> Self {
+        return _drawCircle(x: x, y: y, radius: radius)
+    }
+    
+    private func _drawCircle(x: CGFloat, y: CGFloat, radius: CGFloat) -> Self {
         let mutablePath = CGMutablePath()
         mutablePath.addArc(center: CGPoint(x: x, y: y),
-                     radius: CGFloat(radius),
-                     startAngle: 0,
-                     endAngle: CGFloat.pi * 2,
-                     clockwise: true)
+                           radius: radius,
+                           startAngle: 0,
+                           endAngle: CGFloat.pi * 2,
+                           clockwise: true)
         layer.path = mutablePath
         return self
     }
