17f46db5cd5611faf95c1991895a97e008c24d76
Tasuku Maeda <blogcontact@kuma-de.com>
2019-03-23 10:50:41 +0900

    add CGFloat argument to RectLayerController

--- SimpleCAGraphics/Source/RectLayerController.swift
+++ SimpleCAGraphics/Source/RectLayerController.swift
@@ -17,8 +17,22 @@ public class RectLayerController: LayerController<CAShapeLayer> {
         drawRect(x: x, y: y, width: width, height: height)
     }
     
+    convenience public init(cgX: CGFloat, cgY: CGFloat, cgWidth: CGFloat, cgHeight: CGFloat) {
+        self.init()
+        drawRectCG(x: cgX, y: cgY, width: cgWidth, height: cgHeight)
+    }
+    
     @discardableResult
     public func drawRect(x: Double, y: Double, width: Double, height: Double) -> Self {
+        return _drawRect(x: CGFloat(x), y: CGFloat(y), width: CGFloat(width), height: CGFloat(height))
+    }
+    
+    @discardableResult
+    public func drawRectCG(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -> Self {
+        return _drawRect(x: x, y: y, width: width, height: height)
+    }
+    
+    private func _drawRect(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) -> Self {
         let rect = CGRect(x: x, y: y, width: width, height: height)
         let mutablePath = CGMutablePath()
         mutablePath.addRect(rect)
